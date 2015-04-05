92665e58ffd68e3c9f99c0387bfa4b33c0a8a1a1
TalKashi <shakikashi@gmail.com>
2015-04-06 01:18:35 +0300

    Dynamic Loading Coin Buttons

--- Assets/Scripts/CoinLevelManager.cs
+++ Assets/Scripts/CoinLevelManager.cs
@@ -6,6 +6,11 @@ public class CoinLevelManager : MonoBehaviour
 
     public static CoinLevelManager coinLevelManager;
 
+    
+    public int[] m_coinValues;
+    public int[] m_coinUpgradeCost;
+
+    /*
     public int m_levelOneCoinValue = 2;
     public int m_levelOneUpgradeCost = 100;
 
@@ -13,7 +18,7 @@ public class CoinLevelManager : MonoBehaviour
     public int m_levelTwoUpgradeCost = 200;
 
     public int m_levelThreeCoinValue = 4;
-    public int m_levelThreeUpgradeCost = 300;
+    public int m_levelThreeUpgradeCost = 300;*/
 
     void Awake()
     {
@@ -25,12 +30,17 @@ public class CoinLevelManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }
 
+        if (m_coinValues.Length != m_coinUpgradeCost.Length)
+        {
+            Debug.LogError("m_coinValues.Length != m_coinUpgradeCost.Length! Make sure to setup right!");
+        }
+    }
 
     public int GetLevelValue(int i_level)
     {
-        switch (i_level)
+
+        /*switch (i_level)
         {
             case 1:
                 return m_levelOneCoinValue;
@@ -41,12 +51,20 @@ public class CoinLevelManager : MonoBehaviour
             default:
                 Debug.LogError("The level given (level=" + i_level + ") is not supported!");
                 return -1;
+        }*/
+
+        // Sanity check
+        if (i_level < 0 || i_level >= m_coinValues.Length)
+        {
+            Debug.LogError("Argument Exception: Given out of bound argument (i_level=" + i_level + ";m_coinValues.Length=" + m_coinValues.Length);
+            return -1;
         }
+        return m_coinValues[i_level];
     }
 
     public int GetLevelCost(int i_level)
     {
-        switch (i_level)
+        /*switch (i_level)
         {
             case 1:
                 return m_levelOneUpgradeCost;
@@ -57,6 +75,19 @@ public class CoinLevelManager : MonoBehaviour
             default:
                 Debug.LogError("The level given (level=" + i_level + ") is not supported!");
                 return -1;
+        }*/
+
+        // Sanity check
+        if (i_level < 0 || i_level >= m_coinUpgradeCost.Length)
+        {
+            Debug.LogError("Argument Exception: Given out of bound argument (i_level=" + i_level + ";m_coinUpgradeCost.Length=" + m_coinUpgradeCost.Length);
+            return -1;
         }
+        return m_coinUpgradeCost[i_level];
+    }
+
+    public int GetNumberOfLevels()
+    {
+        return m_coinValues.Length;
     }
 }
--- Assets/Scripts/CoinUpgradeScript.cs
+++ Assets/Scripts/CoinUpgradeScript.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoinUpgradeScript : MonoBehaviour {
+public class CoinUpgradeScript : MonoBehaviour
+{
+
+    public GameObject CoinUpgradeButton;
+
+    void Awake()
+    {
+        gameObject.SetActive(false);
+    }
 
     void Start()
     {
-        gameObject.SetActive(false);
+        init();
     }
 
     public void UpgradeCoin(int i_level)
     {
         GameManagerScript.GameManager.UpgradeCoin(i_level);
+    }
+
+    private void init()
+    {
+        for (int i = 0; i < CoinLevelManager.coinLevelManager.GetNumberOfLevels(); i++)
+        {
+            GameObject newButton = Instantiate(CoinUpgradeButton);
+            UpgradeCoinChecker newButtonScript = newButton.GetComponent<UpgradeCoinChecker>();
+            newButtonScript.m_minExpectedCoinLevel = i;
+            newButtonScript.m_upgradeCost = CoinLevelManager.coinLevelManager.GetLevelCost(i);
+            newButton.transform.SetParent(transform);
+            newButton.SetActive(true);
+        }
+        
     }
 }
